9b29679500bb885dcf7d8a738f9a0eca689a0509
Krishan-max <105154684+Krishan-max@users.noreply.github.com>
2024-10-02 12:19:53 +0530

    Update project.py

--- project.py
+++ project.py
@@ -69,4 +69,6 @@ def ytcomments(l):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001, debug=True)
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if not set
+    app.run(host='0.0.0.0', port=port,debug=True)
+   
